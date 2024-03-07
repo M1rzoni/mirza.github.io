ad0d2ae7cc3212732cc56a8c735f04633c2496a1
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-03-07 09:45:17 +0100

    new commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Contact(){
+    return (
+    <section id="contact" className="contact-container">
+        <div className="main-contact-container">
+            <h1>Contact</h1>
+        </div>
+    </section> 
+    )
+}
+
+export default Contact;
