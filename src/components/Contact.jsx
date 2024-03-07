f63679019db0f62d2abf5d6e900b51d52692dbbe
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-03-07 10:00:06 +0100

    contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,15 @@ function Contact(){
     return (
     <section id="contact" className="contact-container">
         <div className="main-contact-container">
+          <div className="title-container">
             <h1>Contact</h1>
+            <p>Feel free to fill out the form below</p>
+          </div>
+             <div className="contact-content">
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Your E-mail"></input>
+                <textarea name="message" rows={5} placeholder="Your message"></textarea>
+             </div>
         </div>
     </section> 
     )
