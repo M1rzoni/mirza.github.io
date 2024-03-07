bea094f42acdeb50891088f7a224dd7b03c5853e
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-03-07 19:15:16 +0100

    new commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Footer(){
+
+    return (
+        <footer className="footer">
+            Copyright &#169; 2024.
+        </footer>
+    )
+
+}
+
+
+export default Footer;
